9067f4e9bbcea92294e10d45feaaff4b03f84b1e
Morgane Girard <mogi@DESKTOP-QR4BNH8>
2024-10-01 18:09:53 +0200

    Move routes to an other file, add error handling middleware

--- app.mjs
+++ app.mjs
@@ -7,33 +7,46 @@ import bodyParser from 'body-parser';
 import path from 'path';
 import morgan from 'morgan';
 import cors from 'cors';
-import docs from "./docs.mjs";
-import posts from "./routes/posts.mjs";
-// import database from './db/database.mjs';
+
+import documentRoutes from "./routes/documentRoutes.mjs";
 
 const app = express();
 
 app.disable('x-powered-by');
-
 app.set("view engine", "ejs");
 
 app.use(cors());
 app.use(express.static(path.join(process.cwd(), "public")));
 
-// don't show the log when it is test
-if (process.env.NODE_ENV !== 'test') {
-    // use morgan to log at command line
-    app.use(morgan('combined')); // 'combined' outputs the Apache style LOGs
-}
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/posts", posts);
+if (process.env.NODE_ENV !== 'test') {
+  app.use(morgan('combined'));
+}
 
-//delete after
-app.get("/", (req, res) => res.send("test to see"));
-// docs.getAll();
+app.use("/documents", documentRoutes);
+
+app.use((req, res, next) => {
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  if (res.headersSent) {
+      return next(err);
+  }
+
+  res.status(err.status || 500).json({
+      "errors": [
+          {
+              "status": err.status,
+              "detail": err.message
+          }
+      ]
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
