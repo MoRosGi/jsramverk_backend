2974e6250f9f29b533a75415ed2afcc4f2dfd95b
Morgane Girard <mogi@DESKTOP-QR4BNH8>
2024-10-01 18:11:50 +0200

    Change database from sqlite to mongodb via Atlas, create connection

--- db/database.mjs
+++ db/database.mjs
@@ -5,7 +5,7 @@ import { MongoClient, ServerApiVersion } from 'mongodb';
 
 const database = {
   getDb: async function getDb () {
-      let dsn = `mongodb+srv://morganerosegirard:${process.env.DB_PASS}@text-editor.lhjsw.mongodb.net/?retryWrites=true&w=majority&appName=text-editor`;
+      let dsn = `mongodb+srv://${process.env.ATLAS_USERNAME}:${process.env.ATLAS_PASSWORD}@text-editor.lhjsw.mongodb.net/?retryWrites=true&w=majority&appName=text-editor`;
 
       if (process.env.NODE_ENV === 'test') {
           dsn = "mongodb://localhost:27017/test";
@@ -28,31 +28,5 @@ const database = {
       };
   }
 };
-
-// const uri = `mongodb+srv://morganerosegirard:${process.env.DB_PASS}@text-editor.lhjsw.mongodb.net/?retryWrites=true&w=majority&appName=text-editor`;
-
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
 
 export default database;
