7ac77acefd787bc3fcd48aee376fef8ffa1a660d
Morgane Girard <mogi@DESKTOP-QR4BNH8>
2024-10-04 13:47:19 +0200

    Delete config file

--- config.mjs
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config({ path: './.env' });
-
-export const config = {
-    atlasUsername: process.env.ATLAS_USERNAME,
-    atlasPassword: process.env.ATLAS_PASSWORD,
-    port: process.env.PORT,
-    nodeEnv: process.env.NODE_ENV,
-};
