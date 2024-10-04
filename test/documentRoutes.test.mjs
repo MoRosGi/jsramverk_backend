7b782b661426ed6ebeb6276470e892fcacdb7b68
Morgane Girard <mogi@DESKTOP-QR4BNH8>
2024-10-04 17:55:22 +0200

    Remove console log for debugging

--- test/documentRoutes.test.mjs
+++ test/documentRoutes.test.mjs
@@ -52,7 +52,6 @@ describe('documentRoutes', () => {
             this.timeout(5000);
 
             const res = await chai.request.execute(server).get("/documents/");
-            console.log(res.body.data);
             res.body.data.should.have.lengthOf(1);
         });
     });
@@ -87,7 +86,6 @@ describe('documentRoutes', () => {
             this.timeout(5000);
 
             const res = await chai.request.execute(server).get(`/documents/${lastInsertedId}`);
-            console.log(res.body.data);
             res.body.data.title.should.equal("A modified title");
         });
     });
