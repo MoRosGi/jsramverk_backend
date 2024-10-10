83d84d22a1e2c82a70140421552d0b87bba20d11
Morgane Girard <mogi@DESKTOP-QR4BNH8>
2024-10-10 18:51:40 +0200

    Change name for test file

---
+++ tests/documentRoutes.test.mjs
@@ -0,0 +1,95 @@
+import * as dotenv from 'dotenv';
+import request from 'supertest';
+import server from '../app.mjs';
+import database from '../db/database.mjs';
+
+dotenv.config();
+
+process.env.NODE_ENV = 'test';
+
+describe('documentRoutes', () => {
+    let lastInsertedId;
+    let dbClient;
+
+    beforeAll(async () => {
+        const db = await database.getDb();
+        dbClient = db.client;
+    
+        await db.collection.deleteMany({});
+    });
+
+    afterAll(async () => {
+        if (dbClient) {
+            await dbClient.close();
+        }
+        await server.close();
+    });
+
+    describe('POST /documents', () => {
+        it('Should return status 201', async () => {
+            const document = {
+                title: "A Title",
+                content: "A Content"
+            }
+
+            const res = await request(server).post("/documents/").send(document);
+            expect(res.status).toBe(201);
+        });
+    });
+
+    describe('GET /documents', () => {
+        it('Should return status 200', async () => {
+            const res = await request(server).get("/documents/");
+            expect(res.status).toBe(200);
+            lastInsertedId = res.body.data[0]._id;
+        });
+
+        it('Should have a length of 1', async () => {
+            const res = await request(server).get("/documents/");
+            expect(res.body.data.length).toBe(1);
+        });
+    });
+
+    describe('GET /documents/:id', () => {
+        it('Should return status 200', async () => {
+            const res = await request(server).get(`/documents/${lastInsertedId}`);
+            expect(res.status).toBe(200);
+        });
+
+        it('Should be equal to title', async () => {
+            const res = await request(server).get(`/documents/${lastInsertedId}`);
+            expect(res.body.data.title).toBe("A Title");
+        });
+    });
+
+    describe('PUT /documents/:id', () => {
+        it('Should return status 200', async () => {
+            const documentModified = {
+                _id: `${lastInsertedId}`,
+                title: "A modified title",
+                content: "A modified content"
+            }
+
+            const res = await request(server).put(`/documents/${lastInsertedId}`).send(documentModified);
+            expect(res.status).toBe(200);
+        });
+
+        it('Should be equal to modified title', async() => {
+            const res = await request(server).get(`/documents/${lastInsertedId}`);
+            expect(res.body.data.title).toBe("A modified title");
+        });
+    });
+
+    describe('DELETE /documents/:id', () => {
+        it('Should return status 200', async () => {
+            const res = await request(server).delete(`/documents/${lastInsertedId}`);
+            expect(res.status).toBe(200);
+        });
+
+        it('Should be null', async () => {
+            const res = await request(server).get(`/documents/${lastInsertedId}`);
+            expect(res.body.data).toBeNull();
+
+        });
+    });
+});
