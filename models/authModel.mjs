992d7d5c2f456f31c4d2448f25a2d842598fee24
Morgane Girard <mogi@DESKTOP-QR4BNH8>
2024-10-15 18:00:07 +0200

    Organize flow with comments

---
+++ models/authModel.mjs
@@ -0,0 +1,37 @@
+import database from '../db/database.mjs'
+import { ObjectId } from 'mongodb';
+
+const authModel = {
+    // register
+        // body.email body.password
+        // check if email already in database users_collection
+            // bcryptjs hash -> hash body.password
+            // save hash body.password + email in database users_collection
+            // generate JWT token from secret env with jwt.sign and return
+        // check if email in database invite_collection
+            // update users in documents_collection with the body.email
+            // delete the entry for body.email from database invite_collection
+
+    // login
+        // body.email body.password
+        // retrieve email and password from database users_collection
+            // compare body.email vs email database and body.password vs password users_collection with bcryptj compare
+                // generate JWT token from secret env with jwt.sign and return
+
+    // remove
+        // body.email body.password
+        // retrieve email and password from database users_collection
+        // check if user own document in database documents_collection
+            // -> delete owned documents
+        //  delete user from database users_collection
+
+    // invite
+        // document ID + user email + API key from Sigrid/Mailgun
+            // check if user in database users_collection
+                // save user email in database documents_collection
+            // ELSE save in database invites_collection
+
+    // logout ??
+}
+
+export default authModel;
