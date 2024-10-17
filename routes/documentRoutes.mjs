b8e43d547e42c7c8fd10a660872cb88c209db5a3
Morgane Girard <mogi@DESKTOP-QR4BNH8>
2024-10-17 19:28:49 +0200

    Modify get and post route with correct function and parameters

--- routes/documentRoutes.mjs
+++ routes/documentRoutes.mjs
@@ -5,18 +5,7 @@ import documentModel from "../models/documentModel.mjs";
 
 router.get('/', async (req, res, next) => {
     try {
-        const result = await documentModel.getAll();
-        res.status(200).json({
-            data: result
-        });
-    } catch (error) {
-        next(error)
-    }
-});
-
-router.get('/:id', async (req, res, next) => {
-    try {
-        const result = await documentModel.getOne(req.params.id);
+        const result = await documentModel.getAllDataForUser(req.user);
         res.status(200).json({
             data: result
         });
@@ -27,7 +16,7 @@ router.get('/:id', async (req, res, next) => {
 
 router.post("/", async (req, res, next) => {
     try {
-        await documentModel.addOne(req.body);
+        await documentModel.addOne(req.user, req.body);
         res.status(201).json({ 
             data: {
                 msg: "Got a POST request, sending back 201 Created"
@@ -38,30 +27,82 @@ router.post("/", async (req, res, next) => {
     }
 });
 
-router.put("/:id", async (req, res, next) => {
-    try {
-        await documentModel.updateOne(req.body);
-        res.status(200).json({ 
-            data: {
-                msg: "Got a PUT request, sending back 200 Updated"
-            }
-        });
-    } catch (error) {
-        next(error);
-    }
-});
+// router.get("/test", async (req, res, next) => {
+//     try {
+//         res.status(201).json({ 
+//             data: {
+//                 data: req.user
+//             }
+//         });
+//     } catch (error) {
+//         next(error);
+//     }
+// });
 
-router.delete("/:id", async (req, res, next) => {
-    try {
-        await documentModel.deleteOne(req.params.id);
-        res.status(200).json({ 
-            data: {
-                msg: "Got a DELETE request, sending back 200 Deleted"
-            }
-        });
-    } catch (error) {
-        next(error);
-    }
-});
+
+
+
+
+
+// router.get('/', async (req, res, next) => {
+//     try {
+//         const result = await documentModel.getAll();
+//         res.status(200).json({
+//             data: result
+//         });
+//     } catch (error) {
+//         next(error)
+//     }
+// });
+
+// router.get('/:id', async (req, res, next) => {
+//     try {
+//         const result = await documentModel.getOne(req.params.id);
+//         res.status(200).json({
+//             data: result
+//         });
+//     } catch (error) {
+//         next(error)
+//     }
+// });
+
+// router.post("/", async (req, res, next) => {
+//     try {
+//         await documentModel.addOne(req.body);
+//         res.status(201).json({ 
+//             data: {
+//                 msg: "Got a POST request, sending back 201 Created"
+//             }
+//         });
+//     } catch (error) {
+//         next(error);
+//     }
+// });
+
+// router.put("/:id", async (req, res, next) => {
+//     try {
+//         await documentModel.updateOne(req.body);
+//         res.status(200).json({ 
+//             data: {
+//                 msg: "Got a PUT request, sending back 200 Updated"
+//             }
+//         });
+//     } catch (error) {
+//         next(error);
+//     }
+// });
+
+// router.delete("/:id", async (req, res, next) => {
+//     try {
+//         await documentModel.deleteOne(req.params.id);
+//         res.status(200).json({ 
+//             data: {
+//                 msg: "Got a DELETE request, sending back 200 Deleted"
+//             }
+//         });
+//     } catch (error) {
+//         next(error);
+//     }
+// });
 
 export default router;
