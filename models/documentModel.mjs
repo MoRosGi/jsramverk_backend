1f7911e403f8ff5b28394a465f84292755b2f23c
Morgane Girard <mogi@DESKTOP-QR4BNH8>
2024-10-17 19:27:51 +0200

    Add get all data for user, modify addOne with owner and users fields

--- models/documentModel.mjs
+++ models/documentModel.mjs
@@ -1,8 +1,10 @@
 import database from '../db/database.mjs'
 import { ObjectId } from 'mongodb';
 
+import documentService from '../services/documentService.mjs';
+
 const documentModel = {
-    getAll: async function getAll() {
+    getAllDataForUser: async function getAllDataForUser(user) {
         const db = await database.getDb();
 
         try {
@@ -14,66 +16,110 @@ const documentModel = {
         }
     },
 
-    getOne: async function getOne(id) {
-        const db = await database.getDb();
-
-        try {
-            const filter = {_id: ObjectId.createFromHexString(id)};
-            return await db.collectionDocuments.findOne(filter);
-        } catch (e) {
-            throw new Error("Database query failed: " + e.message);
-        } finally {
-            await db.client.close();
-        }
-    },
-
-    addOne: async function addOne(body) {
+    addOne: async function addOne(user, body) {
         const db = await database.getDb();
 
         try {
             const doc = {
                 title: body.title,
                 content: body.content,
+                owner: user.email,
+                users: [ user.email ]
             };
-            return await db.collectionDocuments.insertOne(doc)
+
+            await db.collectionDocuments.insertOne(doc)
+
+            // const insertedDocumentId = documentService.getLastDocumentId();
+            // return { insertedDocumentId: insertedDocumentId };
+
         } catch (e) {
             throw new Error("Database query failed: " + e.message);
         } finally {
             await db.client.close();
         }
     },
+}
+    // addUserToDocument: async function addUserToDocument(user, id)
 
-    updateOne: async function updateOne(body) {
-        const db = await database.getDb();
 
-        try {
-            const filter = {_id: ObjectId.createFromHexString(body["_id"])};
-            const doc = {
-                $set: {
-                    title: body.title,
-                    content: body.content,
-                }
-            };
-            return await db.collectionDocuments.updateOne(filter, doc);
-        } catch (e) {
-            throw new Error("Database query failed: " + e.message);
-        } finally {
-            await db.client.close();
-        }
-    },
 
-    deleteOne: async function deleteOne(id) {
-        const db = await database.getDb();
 
-        try {
-            const filter = {_id: ObjectId.createFromHexString(id)};
-            return await db.collectionDocuments.deleteOne(filter);
-        } catch (e) {
-            throw new Error("Database query failed: " + e.message);
-        } finally {
-            await db.client.close();
-        }
-    },
-};
+
+
+
+// const documentModel = {
+//     getAll: async function getAll() {
+//         const db = await database.getDb();
+
+//         try {
+//             return await db.collectionDocuments.find().toArray();
+//         } catch (e) {
+//             throw new Error("Database query failed: " + e.message);
+//         } finally {
+//             await db.client.close();
+//         }
+//     },
+
+//     getOne: async function getOne(id) {
+//         const db = await database.getDb();
+
+//         try {
+//             const filter = {_id: ObjectId.createFromHexString(id)};
+//             return await db.collectionDocuments.findOne(filter);
+//         } catch (e) {
+//             throw new Error("Database query failed: " + e.message);
+//         } finally {
+//             await db.client.close();
+//         }
+//     },
+
+//     addOne: async function addOne(body) {
+//         const db = await database.getDb();
+
+//         try {
+//             const doc = {
+//                 title: body.title,
+//                 content: body.content,
+//             };
+//             return await db.collectionDocuments.insertOne(doc)
+//         } catch (e) {
+//             throw new Error("Database query failed: " + e.message);
+//         } finally {
+//             await db.client.close();
+//         }
+//     },
+
+//     updateOne: async function updateOne(body) {
+//         const db = await database.getDb();
+
+//         try {
+//             const filter = {_id: ObjectId.createFromHexString(body["_id"])};
+//             const doc = {
+//                 $set: {
+//                     title: body.title,
+//                     content: body.content,
+//                 }
+//             };
+//             return await db.collectionDocuments.updateOne(filter, doc);
+//         } catch (e) {
+//             throw new Error("Database query failed: " + e.message);
+//         } finally {
+//             await db.client.close();
+//         }
+//     },
+
+//     deleteOne: async function deleteOne(id) {
+//         const db = await database.getDb();
+
+//         try {
+//             const filter = {_id: ObjectId.createFromHexString(id)};
+//             return await db.collectionDocuments.deleteOne(filter);
+//         } catch (e) {
+//             throw new Error("Database query failed: " + e.message);
+//         } finally {
+//             await db.client.close();
+//         }
+//     },
+// };
 
 export default documentModel;
