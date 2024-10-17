0680d113b99eff45acfdc1fd5ea9740303523c02
Morgane Girard <mogi@DESKTOP-QR4BNH8>
2024-10-17 10:06:14 +0200

    Commented out result for test

--- routes/authRoutes.mjs
+++ routes/authRoutes.mjs
@@ -3,27 +3,32 @@ const router = express.Router();
 
 import authModel from "../models/authModel.mjs";
 
-router.post('/login', async(req, res, next) => {
+router.post('/register', async (req, res, next) => {
     try {
-        const result = await authModel.login(req.body.email, req.body.password);
+        const result = await authModel.register(req.body.email, req.body.password);
 
         res.status(201).json({
-        success: result.success,
-        token: result.token,
-        message: "Login successful"
-    });
+            data: {
+                success: result.success,
+                // token: result.token,
+                msg: "User registered successfully"
+            }
+        });
     } catch (error) {
         next(error);
     }
 });
 
-router.post('/register', async (req, res, next) => {
+router.post('/login', async(req, res, next) => {
     try {
-        const result = await authModel.register(req.body.email, req.body.password);
+        const result = await authModel.login(req.body.email, req.body.password);
+
         res.status(201).json({
-            success: result.success,
-            token: result.token,
-            message: "User registered successfully"
+            data: {
+                success: result.success,
+                // token: result.token,
+                msg: "Login successful"
+            }
         });
     } catch (error) {
         next(error);
